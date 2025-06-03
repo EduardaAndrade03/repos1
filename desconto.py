e198df0068aa7453ee31f8a236d345eace7ca37b
EduardaAndrade03 <eduardaa2608@gmail.com>
2025-06-02 21:56:19 -0300

    comm2

---
+++ desconto.py
@@ -0,0 +1,4 @@
+preco=int(input("fala um preco ai "))
+desc= preco-(preco*10/100) 
+print('preço original: R$', preco)
+print("preço com desconto: R$", desc)
---
+++ idade.py
@@ -0,0 +1,4 @@
+anoa=int(input("q ano nos tamo? "))
+anon=int(input("q ano c nasceu? "))
+idade=anoa-anon
+print("c tem ",idade, "anos")
